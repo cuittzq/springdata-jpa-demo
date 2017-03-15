787f771ecb22ae575e915d0e7baa010f508ef3e9
tanzhiqiang <tzq139@outlook.com>
2017-03-15 16:25:04 +0800

    集成spring-boot

--- src/main/java/cn/tzq/controller/DeptController.java
+++ src/main/java/cn/tzq/controller/DeptController.java
@@ -2,6 +2,7 @@ package cn.tzq.controller;
 
 import cn.tzq.model.Dept;
 import cn.tzq.service.DeptService;
+import cn.tzq.service.impl.DeptServiceImpl;
 import cn.tzq.utils.RedisTemplateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,18 +14,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/frame")
 public class DeptController {
-    @Autowired
+
     private DeptService deptService;
 
-    @Autowired
     private RedisTemplateUtils redisTemplateUtils;
+
+    @Autowired
+    public DeptController(DeptServiceImpl deptService, RedisTemplateUtils redisTemplateUtils) {
+        this.deptService = deptService;
+        this.redisTemplateUtils = redisTemplateUtils;
+    }
 
     @RequestMapping(value = "/saveUser",
 //            method：  指定请求的method类型， GET、POST、PUT、DELETE等；
             method = RequestMethod.POST,
 //           produces 指定返回的内容类型，仅当request请求头中的(Accept)类型中包含该指定类型才返回
             produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
-
     public String saveUser(@RequestBody Dept dept) {
         boolean issaved = deptService.save(dept);
         return issaved ? "保存成功" : "保存失敗";
@@ -36,7 +41,7 @@ public class DeptController {
     public Dept FindOne(@PathVariable Integer id) {
         Dept redisdept = null;
         try {
-            redisdept =   redisTemplateUtils.get(String.format("Dept_%d", id), Dept.class);
+            redisdept = redisTemplateUtils.get(String.format("Dept_%d", id), Dept.class);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
